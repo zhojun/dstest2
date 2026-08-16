90dbdc6163a3ea6e998d730ea7bf6c9c53232501
周 <39685392@qq.com>
2026-08-16 16:40:45 +0800

    init

---
+++ src/test/java/com/example/myds/MydsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
